c8887add0fa340e776a4bd9681ffe027cc083c6d
Alen <erwinsmithscout1@gmail.com>
2023-08-31 18:47:05 +0530

    add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,15 @@
-import '../styles/globals.css'
- 
+import React from 'react';
+import Head from 'next/head';
+import '../styles/globals.css';
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Weeinge - Get Weeb'd</title>
+        <meta name="description" content="Get'd weebd" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
